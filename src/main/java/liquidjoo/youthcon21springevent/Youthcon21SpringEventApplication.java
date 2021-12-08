d9f049e07ceacdfdcaafec48217f66e6d365d5c3
liquidjoo <liquidjoo@gmail.com>
2021-12-08 21:25:35 +0900

    init : project init

---
+++ src/main/java/liquidjoo/youthcon21springevent/Youthcon21SpringEventApplication.java
@@ -0,0 +1,13 @@
+package liquidjoo.youthcon21springevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Youthcon21SpringEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Youthcon21SpringEventApplication.class, args);
+    }
+
+}
